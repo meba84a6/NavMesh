ec9789d78f6133c9be59804220182bd874f1e615
meba84a6 <meba84a6@gmail.com>
2017-05-16 19:55:20 -0400

    NavMesh Project
    NavMesh Example

---
+++ Assets/PlayerNavigation.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerNavigation : MonoBehaviour {
+
+    public Camera cam;
+    public NavMeshAgent navMeshAgent;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                navMeshAgent.SetDestination(hit.point);
+                if (navMeshAgent.isActiveAndEnabled)
+                {
+                    Debug.Log("ANIMIMATE");
+                }
+            }
+        }
+	}
+}
